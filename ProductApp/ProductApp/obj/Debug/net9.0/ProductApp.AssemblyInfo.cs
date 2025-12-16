d580d1e0b8e15d6b285553e86be3db438a5e55de
Soel Antonio Attala <soelattala@outlook.com>
2025-12-16 16:06:38 -0300

    fix sln docker

--- ProductApp/ProductApp/obj/Debug/net9.0/ProductApp.AssemblyInfo.cs
+++ ProductApp/ProductApp/obj/Debug/net9.0/ProductApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc9b17edf52de33df32bc70ea59e84f72f5d1e67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281d440fe3782a12072bdbbd63fbd6b8232d7ffc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
